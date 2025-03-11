69817c4ea7bf6b4ccc4e0d131fa1b349ef2a3a72
Latex <M.Hassan17877@student.aast.edu>
2025-03-11 22:08:33 +0200

    Add context types

---
+++ client/src/context/types.js
@@ -0,0 +1,37 @@
+// Auth types
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+// Game types
+export const CREATE_GAME = 'CREATE_GAME';
+export const JOIN_GAME = 'JOIN_GAME';
+export const GAME_JOINED = 'GAME_JOINED';
+export const GAME_STARTED = 'GAME_STARTED';
+export const MAKE_MOVE = 'MAKE_MOVE';
+export const MOVE_MADE = 'MOVE_MADE';
+export const GAME_OVER = 'GAME_OVER';
+export const CREATE_AI_GAME = 'CREATE_AI_GAME';
+export const REQUEST_AI_MOVE = 'REQUEST_AI_MOVE';
+export const RESIGN_GAME = 'RESIGN_GAME';
+export const OFFER_DRAW = 'OFFER_DRAW';
+export const DRAW_OFFERED = 'DRAW_OFFERED';
+export const RESPOND_TO_DRAW = 'RESPOND_TO_DRAW';
+export const DRAW_RESPONSE = 'DRAW_RESPONSE';
+export const SET_FEN = 'SET_FEN';
+export const CLEAR_GAME = 'CLEAR_GAME';
+export const GAME_ERROR = 'GAME_ERROR';
+export const PLAYER_DISCONNECTED = 'PLAYER_DISCONNECTED';
+export const PLAYER_RECONNECTED = 'PLAYER_RECONNECTED';
+export const MESSAGE_SENT = 'MESSAGE_SENT';
+export const MESSAGE_RECEIVED = 'MESSAGE_RECEIVED';
+export const CLEAR_MESSAGES = 'CLEAR_MESSAGES';
+export const GET_HISTORY = 'GET_HISTORY';
+export const GET_ACTIVE_GAMES = 'GET_ACTIVE_GAMES';
+export const LOADING = 'LOADING';
+export const CLEAR_LOADING = 'CLEAR_LOADING';
